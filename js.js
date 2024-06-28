b6b3efacd13d6999d1494a883f4f325d7a8109f8
Stas Shynkarenko <stanico@Stass-MacBook-Air.local>
2024-06-28 18:32:36 +0300

    asd

--- js.js
+++ js.js
@@ -6,4 +6,15 @@ clic.addEventListener("click", () => {
 
 });
 
+profil.addEventListener("click", () => {
+    document.getElementById("main").style.display = "none";
+    document.getElementById("profile").style.display = "block";
+
+});
+
+mai.addEventListener("click", () => {
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("main").style.display = "block";
+
+});
 

ce5db559870cd0816b7c9a070b6c29a4056649ad
Stas Shynkarenko <stanico@Stass-MacBook-Air.local>
2024-06-29 11:27:23 +0300

    guifixd

--- js.js
+++ js.js
@@ -18,3 +18,16 @@ mai.addEventListener("click", () => {
 
 });
 
+let usercard = document.getElementById("usercard"); //получаем блок usercard 
+
+let profName = document.createElement('p'); //создаем параграф
+profName.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+//выдем имя, "фамилию", через тире username и код языка
+usercard.appendChild(profName); //добавляем 
+
+let userid = document.createElement('p'); //создаем еще параграф 
+userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
+usercard.appendChild(userid); //добавляем
+

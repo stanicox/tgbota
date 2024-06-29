97adecd8a7d8019e7b7ee7d979878e4c9bdd7b14
Stas Shynkarenko <stanico@Stass-MacBook-Air.local>
2024-06-29 11:30:36 +0300

    guifixd

--- js.js
+++ js.js
@@ -1,23 +1,5 @@
 let tg = window.Telegram.WebApp;
 let clic = document.getElementById("clic");
-
-clic.addEventListener("click", () => {
-    tg.close();
-
-});
-
-profil.addEventListener("click", () => {
-    document.getElementById("main").style.display = "none";
-    document.getElementById("profile").style.display = "block";
-
-});
-
-mai.addEventListener("click", () => {
-    document.getElementById("profile").style.display = "none";
-    document.getElementById("main").style.display = "block";
-
-});
-
 let usercard = document.getElementById("usercard"); //получаем блок usercard 
 
 let profName = document.createElement('p'); //создаем параграф
